904d91cbb403a0b7cf7644d047507f3e8ed4e4d0
morishita.hiromitsu <morishita.hiromitsu@actindi.net>
2019-02-01 18:15:12 +0900

    tslint のエラーを修正

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,24 +1,24 @@
 /* tslint:disable:object-literal-sort-keys */
-import * as path from "path";
-import * as slsw from "serverless-webpack";
+import * as path from 'path';
+import * as slsw from 'serverless-webpack';
 
 module.exports = {
-  mode: slsw.lib.webpack.isLocal ? "development" : "production",
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   entry: slsw.lib.entries,
-  devtool: "source-map",
+  devtool: 'source-map',
   resolve: {
-    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
   },
   output: {
-    libraryTarget: "commonjs",
-    path: path.join(__dirname, ".webpack"),
-    filename: "[name].js",
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
   },
-  target: "node",
+  target: 'node',
   module: {
     rules: [
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: "ts-loader" },
+      { test: /\.tsx?$/, loader: 'ts-loader' },
     ],
   },
 };
